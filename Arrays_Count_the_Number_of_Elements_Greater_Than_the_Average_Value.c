170c6af4105ed0761e3ca006701b8ff81f514eec
MD SHAHA UL ALAM <sahaulalam85@gmail.com>
2026-01-15 23:47:13 +0530

    New Added

---
+++ Arrays_Count_the_Number_of_Elements_Greater_Than_the_Average_Value.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int count = 0;
+    long long sum = 0;
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    double avg = (double)sum / n;
+    for (int i = 0; i < n; i++)
+    {
+        if (avg < a[i])
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
